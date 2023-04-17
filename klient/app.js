492d577793413f74fc0402ec3f8c3e696ae089ea
MrBartek302 <126457666+MrBartek302@users.noreply.github.com>
2023-04-17 11:09:56 +0200

    logowanie_baza

---
+++ klient/app.js
@@ -0,0 +1,21 @@
+async function login(){
+    const formLogin = document.getElementById("login").value
+    const formPass = document.getElementById("pass").value
+
+
+    const data = await fetch(`http://localhost:3000/login/${formLogin}/${formPass}`)
+    const json = await data.json()
+
+    console.log(json)
+
+    localStorage.setItem("login", JSON.stringify(json))
+ 
+}
+
+function adminPage(){
+    const login = JSON.parse(localStorage.getItem("login"))
+    console.log(login)
+
+    if(login.status==false)
+    window.location.href="login.html"
+}
